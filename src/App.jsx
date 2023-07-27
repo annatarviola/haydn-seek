1febdd3d9bdadfa90cb465801c5a2804522533c0
Lily Springer <lilyspringer@hotmail.com>
2023-07-27 10:07:20 -0600

    header updated depending on login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { useContext } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import Header from "./components/Layout/Header";
@@ -8,18 +8,22 @@ import Auth from './components/Forms/Auth'
 import AddLogForm from './components/Forms/AddLogForm'
 import Register from "./components/Forms/Register";
 
+import AuthContext from './store/authContext'
+
 export const baseURL = "http://localhost:8080"
 
 function App() {
+  const authCtx = useContext(AuthContext)
+
   return (
     <>
       <Header />
       <div className="container">
         <Routes>
-          <Route index element={<HomeScreen />} />
-          <Route path="register" element={<Register />}/>
-          <Route path="login" element={<Auth />} />
-          <Route path="add-new-log" element={<AddLogForm />} />
+          <Route index element={authCtx.token ? <HomeScreen /> : <Navigate to='login'/>} />
+          <Route path="register" element={!authCtx.token ? <Register /> : <Navigate to='/' />}/>
+          <Route path="login" element={!authCtx.token ? <Auth /> : <Navigate to='/' />} />
+          <Route path="add-new-log" element={authCtx.token ? <AddLogForm /> : <Auth />} />
         </Routes>
       </div>
     </>
--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,20 +1,37 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
+import AuthContext from "../../store/authContext";
 
 const Header = () => {
+  const authCtx = useContext(AuthContext);
+
   return (
     <div className={styles.header}>
       <h1>Haydn Seek</h1>
       <nav className={styles.nav}>
-        <Link to="">
-          <button className={styles.nav_btn}>Home</button>
-        </Link>
-        <Link to="add-new-log">
-          <button className={styles.nav_btn}>Add New </button>
-        </Link>
-        <Link to="login">
-          <button className={styles.nav_btn}>Login</button>
-        </Link>
+        {authCtx.token ? (
+          <>
+            <Link to="">
+              <button className={styles.nav_btn}>Home</button>
+            </Link>
+            <Link to="add-new-log">
+              <button className={styles.nav_btn}>New Log</button>
+            </Link>
+            <button className={styles.nav_btn} onClick={() => authCtx.logout()}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <Link to="register">
+              <button className={styles.nav_btn}>Sign Up</button>
+            </Link>
+            <Link to="login">
+              <button className={styles.nav_btn}>Login</button>
+            </Link>
+          </>
+        )}
       </nav>
     </div>
   );
